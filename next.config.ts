41a82d08524aeae5e19c525f59e2a9ea1a58affa
ricardo <fkondem@gmail.com>
2025-05-30 09:44:10 +0200

    added stand alone

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig = {
   experimental: {
     appDir: true,  // Ensure App Router is enabled
   },
+  output: 'standalone', // Required for server functions in Amplify
 };
 
 export default nextConfig;
